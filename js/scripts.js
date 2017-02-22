a03bc0fffa52945b4f19b843ecba697b3b59aa55
Chris Wainner and Riley Watts <riley@>
2017-02-22 09:21:33 -0800

    Initial commit, add initial input and logic

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+var testLeapYear = function(input){
+  result = false;
+
+  if(input % 4 === 0){
+    if((input % 100 === 0) && (input % 400 !== 0)){
+      result = false;
+    } else{
+      result = true;
+    }
+  } else{
+    result = false;
+  }
+
+  return result;
+}
+
+
+$(function(){
+  $("#formOne").submit(function(){
+    event.preventDefault();
+    // Get input and parse as an input
+    var input = parseInt($("#inputOne").val());
+    console.log(input);
+    if(isNaN(input)){
+      alert("Please enter a valid number.");
+      location.reload();
+    } else{
+      // Test if it's a leap year
+      var leapYear = testLeapYear(input);
+    }
+
+  });
+});
